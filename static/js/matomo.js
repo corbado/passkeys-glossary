f51dfc43fa88449a4dbe74014aa97d5c023aef5f
Vincent Delitz <vincent.delitz@corbado.com>
2023-07-18 12:00:10 +0200

    add cross domain linking again

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -5,6 +5,7 @@ var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(["setCookieDomain", "*.passkeys-auth.com"]);
 _paq.push(["setDomains", ["*.passkeys-auth.com", "*.passkeys-auth.de", "*.passkeys-glossary.pages.dev"]]);
+_paq.push(["enableCrossDomainLinking"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
