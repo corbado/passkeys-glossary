09eb8849cae9ddcf73128f266b30884152821527
Vincent Delitz <vincent.delitz@corbado.com>
2023-07-18 10:24:19 +0200

    new matomo attempt

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -1,5 +1,6 @@
 /* matomo script */
 
+
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(["setCookieDomain", "*.passkeys-auth.com"]);
@@ -11,4 +12,8 @@ _paq.push(['enableLinkTracking']);
     var u = "//analytics.corbado.com/";
     _paq.push(['setTrackerUrl', u + 'matomo.php']);
     _paq.push(['setSiteId', '6']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
 })();
